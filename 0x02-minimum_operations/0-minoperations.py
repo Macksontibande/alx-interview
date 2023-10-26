e11384ffd41f189bcd2b485416a300778d2a75fa
Macksontibande <Macksontibade@gmail.com>
2023-10-26 01:08:07 +0000

    0. Minimum Operations

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Module for task 0.
+"""
+
+
+def minOperations(n):
+    nOpe = 0
+    minOpe = 2
+    while n > 1:
+        while n % minOpe == 0:
+            nOpe += minOpe
+            n /= minOpe
+        minOpe += 1
+    return nOpe
